38578e49626642d3643cbef66d7aa87d5b24db5a
Sivritkin Dmitriy <dmitriysivritkin@gmail.com>
2024-02-29 19:31:22 +0300

    fix(next): fix error with `useSearchParams`

--- src/app/(auth)/auth/sign-in/page.tsx
+++ src/app/(auth)/auth/sign-in/page.tsx
@@ -1,6 +1,8 @@
 import { SignInForm } from "@/features/auth/sign-in-form.server";
 import { Card, CardContent, CardHeader } from "@/shared/ui/card";
 import Link from "next/link";
+import React, { Suspense } from "react";
+import { Spinner } from "@/shared/ui/spinner";
 
 export default function SignIn() {
   return (
@@ -12,7 +14,9 @@ export default function SignIn() {
           </h1>
         </CardHeader>
         <CardContent className="grid gap-4">
-          <SignInForm />
+          <Suspense fallback={<Spinner />}>
+            <SignInForm />
+          </Suspense>
           <p className="px-0 text-center text-sm text-muted-foreground">
             Нажимая продолжить вы соглашаетсь с{" "}
             <Link
